a549e5fe2264302df05126511facbc5fe44af7b9
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-07-19 23:53:22 +0530

    login_logout_app

---
+++ client/src/components/Username.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Username() {
+    return (
+        <div className="container mx-auto">
+            <div className="flex justify-center items-center h-screen">
+                <div>
+
+                    <div className="tittle flex flex-col items-center">
+                        <h4>Hello Again!</h4>
+                        <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+                            Explore More by connecting with us.
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
